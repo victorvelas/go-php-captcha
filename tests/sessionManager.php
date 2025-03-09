c37cf7268b3647414bc076ebcccd7dd20403ea88
Victor Velasquez <victorvelas.lst@gmail.com>
2025-03-08 20:59:47 -0500

    Fix bug of tmp session folder generation

--- tests/sessionManager.php
+++ tests/sessionManager.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!is_dir('../tmp_sessions')) {
-    mkdir('/../tmp_sessions', 0777);
+if (!is_dir(__DIR__.'../tmp_sessions')) {
+    mkdir(__DIR__.'/../tmp_sessions', 0777);
 }
 session_save_path(__DIR__ . '/../tmp_sessions');
 session_id("GOPHPSESSION");
