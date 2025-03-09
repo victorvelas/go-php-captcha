2683280f522434bde919fdd13076a994709d7f37
Victor Velasquez <victorvelas.lst@gmail.com>
2025-03-08 21:01:59 -0500

    new Fix from error validation directory

--- tests/sessionManager.php
+++ tests/sessionManager.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!is_dir(__DIR__.'../tmp_sessions')) {
+if (!is_dir(__DIR__.'/../tmp_sessions')) {
     mkdir(__DIR__.'/../tmp_sessions', 0777);
 }
 session_save_path(__DIR__ . '/../tmp_sessions');
